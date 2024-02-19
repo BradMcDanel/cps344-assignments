3a1d939d9a0ed00fc46405ddd31b7ee52c92af6f
Brad McDanel <bmcdanel@fandm.edu>
2024-02-19 15:30:16 -0500

    .

--- checksum/src/checksum.c
+++ checksum/src/checksum.c
@@ -31,40 +31,34 @@ uint16_t compute_checksum(uint8_t *udp_packet, uint32_t len) {
   return sum;
 }
 
+void print_packet(uint8_t *udp_packet, uint32_t len) {
+  for (uint32_t i = 8; i < len; i++) {
+    printf("%c", udp_packet[i]);
+  }
+}
+
 int main(void) {
   uint8_t udp_packet[21] = {0x0B, 0xB8, // Source Port (3000),
                             0x00, 0x50, // Destination Port (80),
-                            0x00, 0x15, // Length (8 header + 13 data = 21 byte)
+                            0x00,
+                            0x15, // Length (8 header + 13 data = 21 bytes)
                             0x00, 0x00, // Checksum - needs to be calculated
-                            // Data Portion
+                            // Data Portion - "Hello CPS344!"
                             0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x43, 0x50,
-                            0x53, 0x33, 0x37, 0x33, 0x21};
+                            0x53, 0x33, 0x34, 0x34, 0x21};
 
   uint16_t checksum = compute_checksum(udp_packet, 21);
+
   // Add the checksum to the udp_packet
   udp_packet[6] = (checksum >> 8) & 0xff;
   udp_packet[7] = checksum & 0xff;
 
-  // simulate a network packet loss (via a bit flip)
-  // udp_packet[17] ^= 0x02;
-
-  // calculate the checksum again
-  checksum = compute_checksum(udp_packet, 21);
-
-  // check if the checksum is correct
-  if (checksum == ((udp_packet[6] << 8) | udp_packet[7])) {
-    printf("Checksum is correct\n");
-  } else {
-    printf("Checksum is incorrect: Expected 0x%02x%02x but got 0x%02x%02x\n",
-           udp_packet[6], udp_packet[7], (checksum >> 8) & 0xff,
-           checksum & 0xff);
-  }
+  // Now, we could send it!
+  printf("Checksum: %u\n", checksum);
 
   // Print data portion as char*
   printf("Data: ");
-  for (uint32_t i = 8; i < 21; i++) {
-    printf("%c", udp_packet[i]);
-  }
+  print_packet(udp_packet, 21);
   printf("\n");
 
   return 0;
