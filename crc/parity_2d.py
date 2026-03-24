f91783a48d8d2ca8afe0128dad9593e2fec57a14
Brad McDanel <bmcdanel@fandm.edu>
2026-03-24 12:39:59 -0400

    .

--- crc/parity_2d.py
+++ crc/parity_2d.py
@@ -2,36 +2,18 @@ def compute_parity_bit(data):
     """
     Compute parity bit for even parity (after number of 1's is even)
     """
-    num_ones = 0
-    for i in range(len(data)):
-        if data[i] == 1:
-            num_ones += 1
-
-    if num_ones % 2 == 0:
-        return 0
-    else:
-        return 1
+    pass
 
 
 def parity_check(data):
     """
     Checks if even parity is preserved (number of 1's is even)
     """
-    num_ones = 0
-    for i in range(len(data)):
-        if data[i] == 1:
-            num_ones += 1
-
-    return num_ones % 2 == 0
+    pass
 
 
 if __name__=='__main__':
-    data = [
-        [1, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [0, 1, 1, 1, 0, 0, 1],
-        [1, 0, 1, 0, 1, 1, 0],
-    ]
+    data = [1, 0, 1, 1, 0, 1, 0]
 
 
     parity_bit = compute_parity_bit(data)
@@ -43,4 +25,3 @@ if __name__=='__main__':
     # Simulate error in channel
     data[0] = 0
     print('Data is ok?', parity_check(data))
-
